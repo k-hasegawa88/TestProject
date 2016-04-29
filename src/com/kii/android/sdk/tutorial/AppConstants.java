672958a12f97cd97257631a59e75a4a07ad09ba2
k-hasegawa88 <eldataluta8888@gmail.com>
2016-04-29 16:21:39 +0900

    make progect

---
+++ src/com/kii/android/sdk/tutorial/AppConstants.java
@@ -0,0 +1,11 @@
+package com.kii.android.sdk.tutorial;
+
+import com.kii.cloud.storage.Kii.Site;
+
+public class AppConstants {
+    public static final String APP_ID = "";
+    public static final String APP_KEY = "";
+    public static final Site APP_SITE = Site.US;
+    public static final String APP_BUCKET_NAME = "tutorial";
+    public static final String APP_CHAT_BUCKET_NAME = "chat";
+}
